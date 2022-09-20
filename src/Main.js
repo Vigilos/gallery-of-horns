7621b6ecfc7b8cec5fd05d205b75363305606420
Chris Fanucchi <admin@knightskeep.com>
2022-09-19 18:13:01 -0700

    Displays two animals using static data

--- src/Main.js
+++ src/Main.js
@@ -6,13 +6,40 @@ import sheep from './sheep.jpg';
 
 class Main extends React.Component {
   render() {
+    // const hornedAnimals = [
+    //   {
+    //     title: 'Gazelle',
+    //     imageUrl: { gazelle },
+    //     description:
+    //       'Taken in safari in the Serengeti, Tanzania, taken by Magdalena Kula Manchee on Unsplash',
+    //   },
+    //   {
+    //     title: 'Sheep',
+    //     imageUrl: { sheep },
+    //     description:
+    //       'A stone sheep glares at us in Canada’s Jasper National Park, taken by Paxson Woelber on Unsplash',
+    //   },
+    // ];
+
+    // const images = hornedAnimals.map((title, imageUrl, description) => {
+    //   return (
+    //     <div>
+    //       <HornedBeast
+    //         title={title}
+    //         imageUrl={imageUrl}
+    //         description={description}
+    //       />
+    //     </div>
+    //   );
+    // });
+
     return (
       <>
         <div>
           <HornedBeast
             title="Gazelle"
             imageUrl={gazelle}
-            description="Taken in safari in the Serengeti, Tanzania, taken by Magdalena Kula Manchee on Unsplash "
+            description="Taken in safari in the Serengeti, Tanzania, taken by Magdalena Kula Manchee on Unsplash"
           />
         </div>
         <div>
