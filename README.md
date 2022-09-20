b977f65901812b5a1132108c97982207a102e68b
Chris Fanucchi <admin@knightskeep.com>
2022-09-20 15:10:33 -0700

    Added capability to vote for pics and display hearts

--- README.md
+++ README.md
@@ -9,5 +9,13 @@ A picture gallery of various animals with horns.
 1. Number and name of feature: Display images
    Estimate of time needed to complete: 1 hour
    Start time: 14:07
-   Finish time:
-   Actual time needed to complete:
+   Finish time: 14:30
+   Actual time needed to complete: 23 mins
+
+2. Allow users to vote for their favorite beast
+   Estimate of time needed to complete: 30 hour
+   Start time: 14:50
+   Finish time: 15:05
+   Actual time needed to complete: 15 mins
+
+3.
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { this.props.rating}
-  } 
+    this.state = { rating: this.props.rating };
+  }
+
+  // Handle a click on any card
+  handleClick = () => {
+    this.setState({ rating: this.state.rating + '❤' });
+  };
+
   render() {
     return (
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={this.props.src} />
-      <Card.Body>
-        <Card.Title>{this.props.title}</Card.Title>
-        <Card.Text>{this.props.description}</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+        <Card.Img variant="top" src={this.props.imageUrl} />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>{this.props.description}</Card.Text>
+          <Card.Text>Rating: {this.state.rating}</Card.Text>
+          <Button variant="primary" onClick={this.handleClick}>
+            Vote as Favorite
+          </Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
